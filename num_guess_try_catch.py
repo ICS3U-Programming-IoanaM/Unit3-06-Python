066b7e349107b8e10d594dfe29140358708cf2f7
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-11-04 00:58:43 +0000

    completed num_guess_try_catch.py program

--- num_guess_try_catch.py
+++ num_guess_try_catch.py
@@ -6,8 +6,41 @@
 # random number guessing game with try catch
 
 
-def main():
+import random
 
+def main():
+    # variables
+    user_num_string = input("Please enter a number from 0 to 9: ")
+    rand_num = random.randint(0, 9)
+
+    # exception handling
+    try:
+        user_num_int = int(user_num_string)
+
+    # if the user didn't input a number
+    except ValueError:
+        print("Please input a whole number!")
+
+    # if user imputed an integer
+    else:
+
+        # if user imputed a number out of range
+        if user_num_int < 0 or user_num_int > 9:
+            print("Please enter a number within the specified range!")
+
+        # user imputed number within range
+        else:
+            # user guessed right
+            if user_num_int == rand_num:
+                print(f"Correct! {user_num_int} is the right number!")
+            
+            # user didn't input right number
+            else:
+                print(f"Incorrect! The right number is {rand_num}.")
+
+    # final message to user
+    finally:
+        print("thank you for playing!")
 
 if __name__ == "__main__":
     main()
