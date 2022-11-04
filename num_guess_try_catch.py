0d9848a2df90f65ffcafbe99b81178441629ac91
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-11-04 00:02:40 +0000

    done basic structure fr num_guess_try_catch.py

---
+++ num_guess_try_catch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+#
+# Date: Oct. 31, 2022
+# random number guessing game with try catch
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
